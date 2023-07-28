d0a80fd3317600e298150e01ef91127460afe0c2
Charles <cchester1@angelo.edu>
2023-07-28 11:43:11 -0500

    notes written in newsletter container component for mental map

--- frontend/src/components/NewsLettersContainer.js
+++ frontend/src/components/NewsLettersContainer.js
@@ -1,7 +1,12 @@
 import React from "react";
 import '../stylesheets/main.css';
-
+// import axios
 export default function NewsLettersContainer () {
+      // make axios call to pull news letter data
+      // how to structure the requested data so that it can be properly rendered in component below?
+      // conditionals that provide the next newsletter value with a corresponding html element
+      // for ex: if next newsletter property in the array is img then supply with img element
+
       return (
             <div className="container-fluid newsletter-master ml-3 mr-3">
                   <div
