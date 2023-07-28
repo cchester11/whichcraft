370740e9e8e1749adf0d2801de48fcd33b891655
Charles <cchester1@angelo.edu>
2023-07-28 12:34:20 -0500

    clean up of names of variables in backend folders

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,8 +1,8 @@
 const router = require('express').Router();
 const newsLetterRoutes = require('./newsletters');
-const seedBeer = require('./beers');
+const beerRoutes = require('./beers');
 
 router.use('/newsletters', newsLetterRoutes);
-router.use('/beer', seedBeer);
+router.use('/beers', beerRoutes);
 
 module.exports = router;
