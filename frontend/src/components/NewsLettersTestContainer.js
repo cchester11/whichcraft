cf064795b513a8ec9e691b76929469854ab34413
Charles <cchester1@angelo.edu>
2023-08-02 16:34:51 -0500

    news letters test layout component added

---
+++ frontend/src/components/NewsLettersTestContainer.js
@@ -0,0 +1,194 @@
+import React from "react";
+import '../stylesheets/main.css';
+
+export default function TestNewsLetters() {
+      return (
+            <div cassName="container-fluid newsletter-master ml-3 mr-3">
+                  <div
+                        id="entry-1"
+                        className="container per-newsletter mt-4 ml-2 d-flex flex-column justify-content-center"
+                  >
+                        <div
+                              className="container-fluid d-flex justify-content-center"
+                        >
+                              <h1 className="newsletter-title m-4">Hocus Pocus!</h1>
+                        </div>
+                        <div className="container section-background-1">
+                              <img
+                                    className="container newsletter-section d-flex justify-content-center"
+                                    src="./images/newsletter-header.png"
+                                    alt=""
+                              />
+
+                              <div
+                                    className="container newsletter-section d-flex justify-content-center mt-5"
+                              >
+                                    <h7>
+                                          Pair a Plain, Rosemary, or Cinnamon
+                                          Raisin bagel with cream cheese or
+                                          chive schmear for a tasty breakfast
+                                          on-the-go, or grab our new White
+                                          Cheddar and Prosciutto Grilled Cheese!
+                                          We’re slinging these bad boys all day
+                                          starting from 8am until an hour before
+                                          closing.
+                                    </h7>
+                              </div>
+                        </div>
+                        <div className="container section-background-2">
+                              <img
+                                    className="container newsletter-section d-flex justify-content-center"
+                                    src="./images//newsletter-img-3.png"
+                                    alt=""
+                              />
+                              <div
+                                    className="container newsletter-section d-flex justify-content-center mt-5"
+                              >
+                                    <h7>
+                                          Happy Hour starts at 4 and lasts for
+                                          the entire duration of the game. Look
+                                          for the Longhorns logo on the draft
+                                          board to take $3 off!
+                                    </h7>
+                              </div>
+                        </div>
+                        <div className="container section-background-1">
+                              <img
+                                    className="container newsletter-section d-flex justify-content-center"
+                                    src="./images/newsletter-img-2.png"
+                                    alt=""
+                              />
+                              <div
+                                    className="container newsletter-section d-flex justify-content-center mt-5"
+                              >
+                                    <h7>
+                                          Saloon Door Kemah Suprema- American
+                                          light lager, crisp and refreshing.
+                                          4.7% Bear King Monkey Bridge Hefe-
+                                          This style has a distinct banana
+                                          character with hints of clove, a dry
+                                          finish, full mouthfeel, and high
+                                          carbonation. 5.8% Belching Beaver No
+                                          Worries IPA- Strata, Galaxy, Mosaic,
+                                          Citra, and Amarillo hops all lend
+                                          their characteristics to this
+                                          delightful West Coast IPA.6.2% BrewDog
+                                          USA Hellcat IPA- BrewDog USA partners
+                                          with Iron Maiden to create a​ feisty
+                                          India Pale Lager, citrus-forward with
+                                          an epically complex balance of malts
+                                          and hops. 6% Pinthouse Island Kitty- A
+                                          truly tropical IPA with a full body,
+                                          some underlying hop character, and
+                                          notes of juicy pineapple, tropical
+                                          fruit, coconut, and nutmeg. 6.5% Vista
+                                          Hive Mind- This honey ale was brewed
+                                          with wild yeast and 150 lbs of Central
+                                          Texas Honey donated by 25 Central
+                                          Texas beekeepers. 7.7% Firestone
+                                          Walker Parabola- Intense flavors of
+                                          black cherry, dark chocolate, vanilla
+                                          and roast coffee– yet each annual
+                                          release also bears its own subtle
+                                          imprint. This beer is aged entirely in
+                                          12-year-old bourbon barrels for this
+                                          latest edition. 14%
+                                    </h7>
+                              </div>
+                        </div>
+                  </div>
+                  <div
+                        id="entry-2"
+                        className="container per-newsletter mt-4 ml-2 ml-2 d-flex flex-column justify-content-center"
+                  >
+                        <div
+                              className="container-fluid d-flex justify-content-center"
+                        >
+                              <h1 className="newsletter-title m-4">
+                                    Love is in the Air!
+                              </h1>
+                        </div>
+                        <div className="container section-background-1">
+                              <img
+                                    className="container newsletter-section d-flex justify-content-center"
+                                    src="./images/image-10.png"
+                                    alt=""
+                              />
+
+                              <div
+                                    className="container newsletter-section d-flex justify-content-center mt-5"
+                              >
+                                    <h7>
+                                          Lorem ipsum dolor sit amet consectetur
+                                          adipisicing elit. Quia saepe explicabo
+                                          accusamus et minus cumque? Modi,
+                                          facilis eveniet magni animi corporis
+                                          esse dolor dolorem culpa sint nemo,
+                                          ipsam nostrum ipsum!
+                                    </h7>
+                              </div>
+                        </div>
+                        <div className="container section-background-2">
+                              <div
+                                    className="container newsletter-section d-flex justify-content-center mt-5"
+                              >
+                                    <h7>
+                                          Pinthouse Training Bines- This IPA
+                                          forgoes traditional bitterness in
+                                          favor of a rounded juicy finish and a
+                                          complex olfaction profile of fruit,
+                                          citrus, pine and floral notes from the
+                                          Simcoe and Mosaic Lupulin powder, and
+                                          Citra hops. 7% Austin Eastciders
+                                          Mike’s Hot Honey- Sweet honey, chilis,
+                                          crisp apple with a lingering heat.
+                                          8.3% Austin Eastciders Passionfruit-
+                                          Sweet and tart passion fruit with
+                                          notes of citrus, grapefruit, and a
+                                          tropical twist. 5% 3 Nations Supa
+                                          Fresh IPA- Fresh Strata hop IPA with a
+                                          flavor profile of pineapple citrus and
+                                          stone fruit on dark notes. 6.5% Beer
+                                          Zombies Zombie HWhip Sour- Heavily
+                                          fruited sour with with Blueberries,
+                                          Coconut, Vanilla and marshmallow
+                                          fluff. 8% Beer Zombies Hopshow IPA- A
+                                          hazy tropical juice bomb bursting at
+                                          the seams with mango, peach, pineapple
+                                          & rambutan. 8.3% Weathered Souls
+                                          Murkaveli IPA- DDH DIPA with galaxy
+                                          and citra hops. 9.8% 903 Dr. Seltzer
+                                          Slushy- Dark sweet cherries and spices
+                                          added to a hard seltzer base for a
+                                          sweet slushy sensation. 6.3% Mason Ale
+                                          Works Troublemaker Hazy IPA- This hazy
+                                          IPA is hopped with Citra, Mosaic, and
+                                          Amarillo, providing aromas of
+                                          pineapple, mango, orange, and
+                                          grapefruit. 7% Drekker When Life Gives
+                                          You Prrrts Sour- This Strawberry,
+                                          Guava, Pink Lemonade is tasty,
+                                          incredibly refreshing, and
+                                          lactose-free. 5.9% Drekker Brace for
+                                          the Gs- Hopped with YCH 303, Citra,
+                                          Vic Secret, and Phantasm to really
+                                          blast out the key lime, clementine &
+                                          juicy fruit (like the gum) flavor &
+                                          aromas. 7.2% Drekker Metaphase DDH
+                                          DIPA- Double dry hopped with a pile of
+                                          Citra & Nelson Sauvin. This killer hop
+                                          combo leads to some luscious notes of
+                                          orange, pineapple, papaya, & white
+                                          grape with a beautifully dry finish.
+                                          8.2% Lakewood Mechanical Dragon Gose-
+                                          A magically pink Gose ale made with
+                                          Dragon Fruit and cloudberry, nicely
+                                          balanced with a salty and delightfully
+                                          tart flavor palate. 7%
+                                    </h7>
+                              </div>
+                        </div>
+                  </div>
+            </div>
+      )
+}
