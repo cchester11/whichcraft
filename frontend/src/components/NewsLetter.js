8d0bd668ed3729944286d3c81e127eac264d5b09
Charles <cchester1@angelo.edu>
2023-08-09 12:33:03 -0500

    deleted test newsletter component

--- frontend/src/components/NewsLetter.js
+++ frontend/src/components/NewsLetter.js
@@ -2,13 +2,11 @@ import React from "react";
 import '../stylesheets/main.css';
 import NewsLetterNavButtons from "./NewsLetterNavButtons";
 import NewsLettersContainer from "./NewsLettersContainer";
-// import TestNewsLetters from "./NewsLettersTestContainer";
 
 export default function Newsletter() {
       return (
             <div>
                   <NewsLetterNavButtons />
-                  {/* <TestNewsLetters /> */}
                   <NewsLettersContainer />
             </div>
       )
