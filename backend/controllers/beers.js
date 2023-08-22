e6211ff4db6ea342ee9b26f1b1e147fc8d3a7f4a
Charles <cchester1@angelo.edu>
2023-08-21 19:39:06 -0500

    working on seed beer controller

--- backend/controllers/beers.js
+++ backend/controllers/beers.js
@@ -2,24 +2,34 @@ const data = require('../data/data.json');
 const fs = require('fs');
 const path = require('path');
 
-function getAllBeers (req, res) {
+function getAllBeers(req, res) {
       res.json(data.beer)
 }
 
-function seedBeerController (req, res) {
+function seedBeerController(req, res) {
       let beerPath = path.join(__dirname, '../data/data.json');
       let beerBody = req.body
 
-      data.beer.push(beerBody)
+      try {
+            if (beerBody) {
+                  data.beer.push(beerBody)
 
-      fs.writeFile(beerPath, JSON.stringify(data), () => {
-            console.log(`${data} sent to json storage`)
-      })
+                  fs.writeFile(beerPath, JSON.stringify(data), () => {
+                        console.log(`${data} sent to json storage`)
+                  })
 
-      res.json({ 
-            message: "Successful request",
-            data: req.body
-      })
+                  res.json({
+                        message: "Successful request",
+                        data: req.body
+                  })
+            } else {
+                  res.json({
+                        error: "No body included in your request"
+                  })
+            }
+      } catch (error) {
+            throw new Error(error)
+      }
 }
 
 module.exports = { getAllBeers, seedBeerController };
