0bb33f14468c01c612ef19a1ed913905677cccc2
Charles <cchester1@angelo.edu>
2023-08-22 10:09:01 -0500

    add beer controller error checking

--- backend/controllers/beers.js
+++ backend/controllers/beers.js
@@ -9,9 +9,19 @@ function getAllBeers(req, res) {
 function seedBeerController(req, res) {
       let beerPath = path.join(__dirname, '../data/data.json');
       let beerBody = req.body
+      let beerProperties = []
 
       try {
             if (beerBody) {
+                  Object.entries(beerBody).map(([property, value]) => {
+                        beerProperties.push(property)
+                        if(typeof value !== 'string') {
+                              return 'A string is required as the value type for this input.'
+                        }
+                  })
+
+                  console.log(beerProperties)
+
                   data.beer.push(beerBody)
 
                   fs.writeFile(beerPath, JSON.stringify(data), () => {
