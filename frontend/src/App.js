f592a85998bcd669bacc5ac7a32d9144c46659fb
Charles <cchester1@angelo.edu>
2023-09-02 08:34:39 -0500

    react router initial set up complete

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Main from './components/Main';
+import AdminEntry from './components/AdminEntry';
 
 function App() {
   return (
-    < Main />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Main />} />
+        <Route path='/admin' element={<AdminEntry />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ frontend/src/components/AdminEntry.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function AdminEntry () {
+      return (
+            <div>
+                  "This is the Admin Entry Page"
+            </div>
+      )
+};
