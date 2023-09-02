da96837fdbb05a7827d3bd22636d0d7d46420321
Charles <cchester1@angelo.edu>
2023-09-02 08:15:05 -0500

    done | moving to react router branch | collapsing this branch

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,9 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Main from './components/Main';
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route path='/' Component={Main} />
-      </Switch>
-    </Router>
+    < Main />
   );
 }
 
