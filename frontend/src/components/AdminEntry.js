9de15de019a306b438a9b79b1ad64cc08f7cb0bc
Charles <cchester1@angelo.edu>
2023-09-03 13:23:58 -0500

    login form on admin page written

--- frontend/src/components/AdminEntry.js
+++ frontend/src/components/AdminEntry.js
@@ -1,9 +1,16 @@
 import React from "react";
 
-export default function AdminEntry () {
+export default function AdminEntry() {
       return (
-            <div>
-                  "This is the Admin Entry Page"
+            <div className="container d-flex flex-column" style={{ width:'40%' }}>
+                  <div className="mb-3">
+                        <label for="username_login" className="form-label">Username</label>
+                        <input type="text" className="form-control" id="username_login" placeholder="username" />
+                  </div>
+                  <div className="mb-3">
+                        <label for="password_login" className="form-label">Password</label>
+                        <input type="password" className="form-control" id="password_login" placeholder="password" />
+                  </div>
             </div>
       )
 };
