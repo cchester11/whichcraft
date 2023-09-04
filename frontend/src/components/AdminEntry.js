b26cdb4f37329aca94b273e0976726e7a7faa13e
Charles <cchester1@angelo.edu>
2023-09-04 10:37:22 -0500

    more work on adminEntry component logic

--- frontend/src/components/AdminEntry.js
+++ frontend/src/components/AdminEntry.js
@@ -5,6 +5,18 @@ export default function AdminEntry() {
       const [username, setUsername] = useState('')
       const [password, setPassword] = useState('')
 
+      const handleUsername = (event) => {
+            const inputVal = event.target.value
+
+            setUsername(inputVal)
+      };
+
+      const handlePassword = (event) => {
+            const inputVal = event.target.value;
+
+            setPassword(inputVal)
+      };
+
       const handleLogin = (event, username, password) => {
             event.preventDefault()
 
@@ -14,19 +26,19 @@ export default function AdminEntry() {
             }, () => {
                   console.log(Response)
             })
-      }
+      };
 
       return (
             <div className="container d-flex flex-column" style={{ width:'40%' }}>
                   <div className="mb-3">
                         <label htmlFor="username_login" className="form-label">Username</label>
-                        <input type="text" className="form-control" id="username_login" placeholder="username" />
+                        <input type="text" className="form-control" id="username_login" placeholder="username" onChange={handleUsername}/>
                   </div>
                   <div className="mb-3">
                         <label htmlFor="password_login" className="form-label">Password</label>
-                        <input type="password" className="form-control" id="password_login" placeholder="password" />
+                        <input type="password" className="form-control" id="password_login" placeholder="password" onChange={handlePassword}/>
                   </div>
-                  <button type="submit" className="btn btn-large btn-primary" onSubmit={handleLogin()}>Login</button>
+                  <button type="submit" className="btn btn-large btn-primary" onSubmit={handleLogin(username, password)}>Login</button>
             </div>
       )
 };
