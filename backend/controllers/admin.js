34c2de72c4f08f2102ada8d2dee7f504ad226d13
Charles <cchester1@angelo.edu>
2023-09-05 15:17:51 -0500

    error handling

--- backend/controllers/admin.js
+++ backend/controllers/admin.js
@@ -3,17 +3,25 @@ const path = require('path');
 
 const loginAuth = (req, res) => {
       const filePath = path.join(__dirname, '../data/admin.json');
-      const adminData = fs.readFileSync(filePath, 'utf-8')
 
-      console.log(req.body)
+      try {
+            const adminData = fs.readFileSync(filePath, 'utf-8')
 
-      const response = JSON.parse(adminData);
-      console.log(req.body);
+            console.log(req.body)
 
-      res.json({
-            message: "Response",
-            data: response
-      })
+            const response = JSON.parse(adminData);
+            console.log(req.body);
+
+            res.json({
+                  message: "Response",
+                  data: response
+            })
+      }
+      catch (error) {
+            res.status(500).json({
+                  error: error.message
+            })
+      }
 };
 
 module.exports = loginAuth;
