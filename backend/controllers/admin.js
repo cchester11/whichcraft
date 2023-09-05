a92b8b4cdbf0ef7ca4602bddffe1c537bb61d394
Charles <cchester1@angelo.edu>
2023-09-05 13:47:59 -0500

    comment out imports for admin controller | add serial key for json

--- backend/controllers/admin.js
+++ backend/controllers/admin.js
@@ -1,6 +1,6 @@
-const admin = require('../data/admin.json');
-const fs = require('fs');
-const path = require('path');
+// const admin = require('../data/admin.json');
+// const fs = require('fs');
+// const path = require('path');
 
 const loginAuth = (req, res) => {
       console.log(req.body)
