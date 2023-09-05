28d0e5813c4e01798a8bb918bcdfa5647da7ec54
Charles <cchester1@angelo.edu>
2023-09-05 15:57:13 -0500

    refactored handleLogin function

--- frontend/src/components/AdminEntry.js
+++ frontend/src/components/AdminEntry.js
@@ -18,12 +18,12 @@ export default function AdminEntry() {
       };
 
       const handleLogin = async (username, password) => {
-            await axios.post('http://localhost:3001/admin/login', {
+            const response = await axios.post('http://localhost:3001/admin/login', {
                   username: username,
                   password: password
-            }, () => {
-                  console.log(Response)
             })
+
+            console.log(response.data.data)
       };
 
       return (
