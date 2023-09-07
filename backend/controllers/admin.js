258ffa29f6678185c5285290ad6bde6101e70935
Charles <cchester1@angelo.edu>
2023-09-07 16:31:25 -0500

    login logic good. moved log in credentials to env variable

--- backend/controllers/admin.js
+++ backend/controllers/admin.js
@@ -1,35 +1,29 @@
-const fs = require('fs');
-const path = require('path');
+require('dotenv').config({ path: '../.env'})
 
 const loginAuth = (req, res) => {
-      const filePath = path.join(__dirname, '../data/admin.json');
-
       try {
-            const adminData = fs.readFileSync(filePath, 'utf-8')
-            const authCredentials = JSON.parse(adminData);
-
-            const authenticatedUsername = authCredentials.username;
-            const authenticatedPassword = authCredentials.password;
-
-            if(authenticatedUsername === req.body.username && authenticatedPassword === req.body.password) {
+            const authUsername = process.env.ADMIN_USERNAME;
+            const authPassword = process.env.ADMIN_PASSWORD;
+            if (
+                  authUsername === req.body.username &&
+                  authPassword === req.body.password
+            ) {
                   res.json({
-                        message: "Attempted login successful",
-                        date: new Date,
-                        loggedIn: true
-                  })
+                        message: 'Attempted login successful',
+                        date: new Date(),
+                        loggedIn: true,
+                  });
             } else {
                   res.json({
-                        message: "Attempted login unsuccessful",
-                        date: new Date,
-                        loggedIn: false
-                  })
+                        message: 'Attempted login unsuccessful',
+                        date: new Date(),
+                        loggedIn: false,
+                  });
             }
-
-      }
-      catch (error) {
+      } catch (error) {
             res.status(500).json({
-                  error: error.message
-            })
+                  error: error.message,
+            });
       }
 };
 
