ef7ab23cf95d24b2739364ce7785c238e35e0eff
Charles <cchester1@angelo.edu>
2023-09-27 15:13:59 -0500

    comments written on workshop page for implementing logout logic

--- frontend/src/components/AdminWorkshop.js
+++ frontend/src/components/AdminWorkshop.js
@@ -2,7 +2,16 @@ import React from "react";
 import '../stylesheets/main.css';
 
 export default function AdminWorkshop () {
+      // onClick function for logout button
+      // sends a request to an endpoint on the server that will destroy the server token
+      // sends back a response
+      // empty local storage
+      // redirect to home page 
+
       return (
-            <div>Admin Workshop</div>
+            <div>
+                  <h1>Admin Workshop</h1>
+                  <button className="btn btn-large btn-primary">Logout</button>
+            </div>
       )
 }
