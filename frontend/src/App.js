b51481a14f7493cb426467ea198a6a3c7ec78098
Charles <cchester1@angelo.edu>
2023-11-17 10:31:48 -0600

    placed all crud workshop components under the protected routes route in the app file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,10 @@ import Main from './components/Main';
 import AdminLogin from './components/admin/AdminLogin';
 import AdminWorkshop from './components/admin/AdminWorkshop';
 import PrivateRoutes from './utilities/PrivateRoutes';
+import CreateBeer from './components/admin/workshopComponents/CreateBeer';
+import DeleteBeer from './components/admin/workshopComponents/DeleteBeer';
+import CreateNewsletter from './components/admin/workshopComponents/CreateNewsletter';
+import DeleteNewsletter from './components/admin/workshopComponents/DeleteNewsletter';
 
 function App() {
   return (
@@ -13,6 +17,10 @@ function App() {
         <Route path='/admin' element={<AdminLogin />} />
         <Route element={<PrivateRoutes />}>
           <Route path='/workshop' element={<AdminWorkshop />} />
+          <Route path='/workshop/createbeer' element={<CreateBeer />} />
+          <Route path='/workshop/deletebeer' element={<DeleteBeer/>} />
+          <Route path='/workshop/createnewsletter' element={<CreateNewsletter />} />
+          <Route path='/workshop/deletenewsletter' element={<DeleteNewsletter />} />
         </Route>
       </Routes>
     </BrowserRouter>
