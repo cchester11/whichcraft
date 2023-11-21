a792f264f4ad9a45def30993195c2d67c2a5c9f7
Charles <cchester1@angelo.edu>
2023-11-21 14:11:15 -0600

    instructions written for update to delete tap feature

--- frontend/src/components/admin/workshopComponents/Deletetap.js
+++ frontend/src/components/admin/workshopComponents/Deletetap.js
@@ -8,14 +8,17 @@ import axios from "axios";
 // 1. Either the admin can type in a tap name to the form input or a dropdown list can populate and the admin can select what tap to delete from the dropdown
 // 2. A submit handler that will send a request to the delete tap api endpoint 
 // Notes:
-// Change form to the search implementation so that as the admin types in a title, it will be found for him
+// 1. Change form to the search implementation so that as the admin types in a title, it will be found for him
+//    Implementation:
+//          1. Write a get api in taps controllers that will grab all beer titles stored in json
+//          2. Call that api endpoint here
+//          3. Place returned values in an array
+//          4. Loop through that array to place options in the datalist element
 
 export default function Deletetap () {
       const [tapToDelete, setTapToDelete] = useState("");
 
       const submitHandler = async () => {
-            // I need to save the value the admin places in the input below to the tapToDelete state
-            // then I need to send an axios request to /taps/deletetap
             try {
                   if(tapToDelete.trim() === "") {
                         alert("Please enter a tap name");
@@ -41,7 +44,8 @@ export default function Deletetap () {
                   <div className="mb-3">
                         <label htmlFor="deleteTapForm" className="form-label">Delete Tap</label>
                         <input 
-                              className="form-control form-control-sm" 
+                              className="form-control form-control-sm"
+                              list="titleList"
                               id="deleteTapForm" 
                               type="text" 
                               placeholder="Ex: Blue Owl Cherry Blossom" 
@@ -50,6 +54,9 @@ export default function Deletetap () {
                               onChange={(event) => setTapToDelete(event.target.value)}
                         >
                         </input>
+                        <datalist id="titleList">
+                              {/* map through all titles from json here */}
+                        </datalist>
                         <button className="btn btn-large btn-primary" onClick={submitHandler}>Submit</button>
                   </div>
             </div>
