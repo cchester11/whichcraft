7eecf4dbcd136a00018ec0340f865d705af585e0
Charles <cchester1@angelo.edu>
2023-12-03 17:56:45 -0600

    changed up newsletter nav element

--- frontend/src/components/newsletters/NewsLetterNavButtons.js
+++ frontend/src/components/newsletters/NewsLetterNavButtons.js
@@ -3,44 +3,24 @@ import '../../stylesheets/main.css';
 
 export default function NewsLetterNavButtons() {
       return (
-            <nav
-                  className="d-flex justify-content-end mt-1 nav-buttons-master align-items-center"
-                  id="newsLetterNav"
-            >
-                  <h3>Select Article</h3>
-                  <ul className="newsletter-navbuttons-container">
-                        <ul>
-                              <a
-                                    className="newsletter-nav-button btn btn-outline-warning"
-                                    href="#entry-1"
-                              >1</a>
+            // <nav
+            //       className="d-flex justify-content-end mt-1 nav-buttons-master align-items-center"
+            //       id="newsLetterNav"
+            // >
+            <div>
+                  <nav id="newsLetterNav" class="navbar  newsletters-navbar px-3 mb-3 d-flex justify-content-end mt-1 nav-buttons-master align-items-center">
+                        <ul class="nav nav-pills">
+                              <li class="nav-item dropdown">
+                                    <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false">Select Article</a>
+                                    <ul class="dropdown-menu">
+                                          <li><a class="dropdown-item" href="#scrollspyHeading3">Third</a></li>
+                                          <li><a class="dropdown-item" href="#scrollspyHeading4">Fourth</a></li>
+                                          <li><hr class="dropdown-divider"></hr></li>
+                                          <li><a class="dropdown-item" href="#scrollspyHeading5">Fifth</a></li>
+                                    </ul>
+                              </li>
                         </ul>
-                        <ul>
-                              <a
-                                    className="newsletter-nav-button btn btn-outline-warning"
-                                    href="#entry-2"
-                              >2
-                              </a>
-                        </ul>
-                        <ul>
-                              <a
-                                    className="newsletter-nav-button btn btn-outline-warning"
-                                    href="#entry-3"
-                              >3</a>
-                        </ul>
-                        <ul>
-                              <a
-                                    className="newsletter-nav-button btn btn-outline-warning"
-                                    href="#entry-4"
-                              >4</a>
-                        </ul>
-                        <ul>
-                              <a
-                                    className="newsletter-nav-button btn btn-outline-warning"
-                                    href="#entry-5"
-                              >5</a>
-                        </ul>
-                  </ul>
-            </nav>
+                  </nav>
+            </div>
       )
 }
