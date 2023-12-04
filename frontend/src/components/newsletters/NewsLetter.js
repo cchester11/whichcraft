46becaf1adf18934f329ffab7d96f9d8f83060b5
Charles <cchester1@angelo.edu>
2023-12-03 23:57:35 -0600

    removed newsletter nav

--- frontend/src/components/newsletters/NewsLetter.js
+++ frontend/src/components/newsletters/NewsLetter.js
@@ -1,14 +1,12 @@
 import React from "react";
 import '../../stylesheets/main.css';
 import NewsLettersHeader from "./NewsLettersHeader";
-import NewsLetterNavButtons from "./NewsLetterNavButtons";
 import NewsLettersContainer from "./NewsLettersContainer";
 
 export default function Newsletter() {
       return (
             <div>
                   <NewsLettersHeader />
-                  <NewsLetterNavButtons />
                   <NewsLettersContainer />
             </div>
       )
