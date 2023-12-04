36fe4ca12847598a7965b8867eac75fe4d8bd82b
Charles <cchester1@angelo.edu>
2023-12-03 18:13:45 -0600

    ..

--- frontend/src/components/newsletters/NewsLetterNavButtons.js
+++ frontend/src/components/newsletters/NewsLetterNavButtons.js
@@ -7,7 +7,7 @@ export default function NewsLetterNavButtons() {
                   <nav id="newsLetterNav" class="navbar  newsletters-navbar px-3 mb-3 d-flex justify-content-end mt-1 nav-buttons-master align-items-center">
                         <ul class="nav nav-pills">
                               <li class="nav-item dropdown">
-                                    <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false">Select Article</a>
+                                    <a class="nav-link text-dark dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false">Select Article</a>
                                     <ul class="dropdown-menu">
                                           <li><a class="dropdown-item" href="#entry-1">Hocus Pocus</a></li>
                                           <li><a class="dropdown-item" href="#entry-2">Merry Christmas</a></li>
