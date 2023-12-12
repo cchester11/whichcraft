8f295221b3d7caa29ee6e4818dc1c099db42db77
Charles <cchester1@angelo.edu>
2023-12-12 16:32:57 -0600

    changes written to add image component | test later

--- frontend/src/components/admin/workshopComponents/CreateNewsletter.js
+++ frontend/src/components/admin/workshopComponents/CreateNewsletter.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect} from "react";
+import React, { useState } from "react";
 import AddTitle from "./addNewsletterForms/AddTitle";
 import logout from "../../../utilities/logout";
 import ElementSelection from "./addNewsletterForms/ElementSelection";
--- frontend/src/components/admin/workshopComponents/addNewsletterForms/AddImage.js
+++ frontend/src/components/admin/workshopComponents/addNewsletterForms/AddImage.js
@@ -3,37 +3,70 @@ import React, { useState } from "react";
 export default function AddImage(props) {
       const [imageItem, setImageItem] = useState("");
 
+      // file change function used when a file is placed into the 'choose file' input
+      const handleFileChange = (event) => {
+            const file = event.target.files[0];
+            setImageItem(file);
+      };
+
+      // handle submission function runs when the parent form is submitted
+      const handleUpload = async (event) => {
+            event.preventDefault();
+
+            // initializing FormData will set a new object which will store the file selected and input by the admin
+            const formData = new FormData();
+            formData.append("imageInput", imageItem);
+
+            try {
+                  const response = await fetch("http://localhost:3001/images/uploadimage", {
+                        method: "post",
+                        body: formData
+                  })
+
+                  console.log(response);
+
+                  props.newsLetterOb.push({
+                        imageItem: {
+                              element: "image",
+                              image: `images/${imageItem.name}`,
+                              resize: true
+                        }
+                  });
+
+                  props.setCurrentForm("ElementSelection");
+            } catch (error) {
+                  console.error("Error uploading image:", error)
+            }
+      };
+
       return (
             <div>
                   <h1 className="container-fluid d-flex justify-content-center tap-menu-header mt-5">Add Newsletter</h1>
                   <div className="m-4">
-                        <div class="mb-3">
+                        <div className="mb-3">
                               <label htmlFor="formFile" className="form-label">Add Image</label>
-                              <input 
-                                    className="form-control" 
-                                    type="file" 
-                                    id="formFile"
-                                    value={imageItem}
-                                    onChange={(event) => setImageItem(event.target.value)}
+                              <form 
+                                    action="/images/uploadimage" 
+                                    encType="multipart/form-data" 
+                                    method="post"
+                                    onSubmit={handleUpload}
                               >
-                              </input>
+                                    <input
+                                          className="form-control"
+                                          type="file"
+                                          id="formFile"
+                                          name="imageInput"
+                                          onChange={handleFileChange}
+                                    >
+                                    </input>
+                                    <button
+                                          type="submit"
+                                          className="btn btn-large btn-primary m-3"
+                                    >
+                                          Submit
+                                    </button>
+                              </form>
                         </div>
-                        <button
-                              className="btn btn-large btn-primary m-3"
-                              onClick={() => {
-                                    /* dont forget to define image name*/
-                                    props.newsLetterOb.push({
-                                          imageItem: {
-                                                element: "image",
-                                                image: `images/${imageItem}`,
-                                                resize: true
-                                          }
-                                    })
-                                    props.setCurrentForm("ElementSelection")
-                              }}
-                        >
-                              Submit
-                        </button>
                   </div>
             </div>
       )
