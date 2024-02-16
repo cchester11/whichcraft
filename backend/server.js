3edc3cd73e589c0bd2ddeb32cda9bbe0922e4fb1
Charles <cchester1@angelo.edu>
2024-02-16 08:50:31 -0600

    ..

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const bodyParser = require('body-parser');
-
+const port =  process.env.PORT || 3001;
 const routes = require('./routes');
 
 app.use(cors());
@@ -10,6 +10,6 @@ app.use(bodyParser.json());
 
 app.use('/', routes);
 
-app.listen(3001, () => {
-      console.log('app running on PORT 3001')
+app.listen(port, () => {
+      console.log('app running on PORT ' + port)
 });
